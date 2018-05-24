eb8da8c137acabb6822ef45685ea276b29a44b0f
Robin <robin_mvc@163.com>
2018-05-24 14:56:03 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/pingan/com/coolweather/db/County.java
@@ -0,0 +1,8 @@
+package pingan.com.coolweather.db;
+
+/**
+ * @author Robin
+ * @date 2018/5/24 14:49
+ */
+public class County {
+}
---
+++ app/src/main/java/pingan/com/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package pingan.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @author Robin
+ * @date 2018/5/24 14:47
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
